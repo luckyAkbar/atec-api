f299a0886a7cec581221480eb3175131686cf0cf
lucky akbar <luckyakbar1509@gmail.com>
2023-11-29 18:18:17 +0700

    feat: custom error to ease http resp generation

---
+++ internal/common/error.go
@@ -0,0 +1,28 @@
+package common
+
+import (
+	stdhttp "github.com/sweet-go/stdlib/http"
+)
+
+// Error satisfy default Error interface
+type Error struct {
+	Message string
+	Cause   error
+	Code    int
+	Type    error
+}
+
+// Error error message
+func (e Error) Error() string {
+	return e.Message
+}
+
+// GenerateStdlibHTTPResponse added functionality to self returning appropriate error message, setting accurate http status codes, and error code
+func (e *Error) GenerateStdlibHTTPResponse() *stdhttp.StandardResponse {
+	return &stdhttp.StandardResponse{
+		Success: false,
+		Message: e.Message,
+		Status:  e.Code,
+		Error:   e.Type.Error(),
+	}
+}
