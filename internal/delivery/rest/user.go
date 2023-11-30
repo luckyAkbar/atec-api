fe76f4fbdb1fe5ff32b31c47f2775d59b96e55b6
lucky akbar <luckyakbar1509@gmail.com>
2023-11-30 18:31:53 +0700

    feat: register account verification to router

--- internal/delivery/rest/user.go
+++ internal/delivery/rest/user.go
@@ -37,3 +37,31 @@ func (s *service) handleSignUp() echo.HandlerFunc {
 		}
 	}
 }
+
+func (s *service) handleAccountVerification() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		var input = struct {
+			Request   *model.AccountVerificationInput `json:"request"`
+			Signature string                          `json:"signature"`
+		}{}
+		if err := c.Bind(&input); err != nil {
+			return s.apiResponseGenerator.GenerateEchoAPIResponse(c, ErrBadRequest.GenerateStdlibHTTPResponse(nil), nil)
+		}
+
+		success, failed, custerr := s.userUsecase.VerifyAccount(c.Request().Context(), input.Request)
+		switch custerr.Type {
+		default:
+			return s.apiResponseGenerator.GenerateEchoAPIResponse(c, custerr.GenerateStdlibHTTPResponse(failed), nil)
+		case usecase.ErrInternal:
+			logrus.WithContext(c.Request().Context()).WithError(custerr.Cause).Error("failed to handle account verification")
+			return s.apiResponseGenerator.GenerateEchoAPIResponse(c, ErrInternal.GenerateStdlibHTTPResponse(nil), nil)
+		case nil:
+			return s.apiResponseGenerator.GenerateEchoAPIResponse(c, &stdhttp.StandardResponse{
+				Success: true,
+				Message: "success",
+				Status:  http.StatusOK,
+				Data:    success,
+			}, nil)
+		}
+	}
+}
