689a99cdcc75a19354a30418588e6337985b3ca3
lucky akbar <luckyakbar1509@gmail.com>
2023-11-30 18:35:18 +0700

    feat: pin validation for account activation

--- internal/model/user.go
+++ internal/model/user.go
@@ -52,13 +52,43 @@ type SignUpResponse struct {
 	RemainingAttempts int       `json:"remainingAttempts"`
 }
 
+// AccountVerificationInput will be the request format to verify account pin
+type AccountVerificationInput struct {
+	PinValidationID uuid.UUID `json:"pinValidationID" validate:"required,uuid4"`
+	Pin             string    `json:"pin" validate:"required"`
+}
+
+// Validate validates struct
+func (a *AccountVerificationInput) Validate() error {
+	return validator.Struct(a)
+}
+
+// SuccessAccountVerificationResponse will be returned when pin varification is successfull
+type SuccessAccountVerificationResponse struct {
+	ID        uuid.UUID `json:"id"`
+	Email     string    `json:"email"`
+	Username  string    `json:"username"`
+	IsActive  bool      `json:"isActive"`
+	Role      Role      `json:"role"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+	DeletedAt null.Time `json:"deletedAt,omitempty"`
+}
+
+// FailedAccountVerificationResponse will be returned when pin varification is failed
+type FailedAccountVerificationResponse struct {
+	RemainingAttempts int `json:"remainingAttempts"`
+}
+
 // UserUsecase user's usecase
 type UserUsecase interface {
 	SignUp(ctx context.Context, input *SignUpInput) (*SignUpResponse, *common.Error)
+	VerifyAccount(ctx context.Context, input *AccountVerificationInput) (*SuccessAccountVerificationResponse, *FailedAccountVerificationResponse, *common.Error)
 }
 
 // UserRepository user's repository
 type UserRepository interface {
 	Create(ctx context.Context, user *User, tx *gorm.DB) error
 	FindByEmail(ctx context.Context, email string) (*User, error)
+	UpdateActiveStatus(ctx context.Context, id uuid.UUID, status bool) (*User, error)
 }
