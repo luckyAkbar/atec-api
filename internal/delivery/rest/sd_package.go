266bcd762549558eaca16e08acbe4162169dd72a
lucky akbar <luckyakbar1509@gmail.com>
2023-12-13 17:37:08 +0700

    feat: activate and deactivate sd package

--- internal/delivery/rest/sd_package.go
+++ internal/delivery/rest/sd_package.go
@@ -169,3 +169,37 @@ func (s *service) handleUndoDeleteSDPackage() echo.HandlerFunc {
 		}
 	}
 }
+
+func (s *service) handleChangeSDPackageActivationStatus() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		type body struct {
+			ActivationStatus bool `json:"activationStatus"`
+		}
+		input := struct {
+			Request   *body  `json:"request"`
+			Signature string `json:"signature"`
+		}{}
+
+		id := c.Param("id")
+		packageID, parsingErr := uuid.Parse(id)
+		if err := c.Bind(&input); err != nil || parsingErr != nil {
+			return s.apiResponseGenerator.GenerateEchoAPIResponse(c, ErrBadRequest.GenerateStdlibHTTPResponse(nil), nil)
+		}
+
+		resp, custerr := s.sdpackageUsecase.ChangeSDPackageActiveStatus(c.Request().Context(), packageID, input.Request.ActivationStatus)
+		switch custerr.Type {
+		default:
+			return s.apiResponseGenerator.GenerateEchoAPIResponse(c, custerr.GenerateStdlibHTTPResponse(nil), nil)
+		case usecase.ErrInternal:
+			logrus.WithContext(c.Request().Context()).WithError(custerr.Cause).Error("failed to change sd package activation status")
+			return s.apiResponseGenerator.GenerateEchoAPIResponse(c, ErrInternal.GenerateStdlibHTTPResponse(nil), nil)
+		case nil:
+			return s.apiResponseGenerator.GenerateEchoAPIResponse(c, &stdhttp.StandardResponse{
+				Success: true,
+				Message: "success",
+				Status:  http.StatusOK,
+				Data:    resp,
+			}, nil)
+		}
+	}
+}
