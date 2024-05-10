af22f2d1ffd109d938f94070899571b98b071bf6
luckyakbar <lucky.akbar@jatis.com>
2024-05-10 09:15:17 +0700

    feat: implement cache on auth creds

--- internal/model/cacher.go
+++ internal/model/cacher.go
@@ -5,6 +5,10 @@ import (
 	"time"
 )
 
+// NilKey is used to indicate that this value / cache is intended to be treated as nil
+// and different from not found from cache
+const NilKey = "NIL"
+
 // Cacher :nodoc:
 type Cacher interface {
 	Get(ctx context.Context, key string) (string, error)
